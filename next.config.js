eeebcd36fba632483372339b1344dd908a4f872a
Anthony-333 <anthonyoling333@gmail.com>
2021-08-24 15:27:54 +0800

    commit on main

--- next.config.js
+++ next.config.js
@@ -2,9 +2,4 @@ module.exports = {
   images: {
     domains: ["www.google.com", "www.pngkey.com"],
   },
-
-  env: {
-    API_KEY: 'AIzaSyBxd-CDu1NErrnpSXRSdgGNazNVpnjy5ME',
-    CONTEXT_KEY: '029dbfa01682b0421'
-  },
 };
